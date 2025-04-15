3fcef900059156f3a6723804f110b227f9c2d116
HiWill <hiwill@HiWilldeMac-Studio.local>
2025-04-15 10:08:28 +0800

    fix: radar chart renderer error cached xLabels.

--- Source/Charts/Renderers/RadarChartRenderer.swift
+++ Source/Charts/Renderers/RadarChartRenderer.swift
@@ -85,12 +85,20 @@ open class RadarChartRenderer: LineRadarRenderer
 
         let prefix: String = chart.data?.accessibilityEntryLabelPrefix ?? "Item"
         let description = dataSet.label ?? ""
+        
+        var xLabels: [String] = {
+            guard let formatter = chart.xAxis.valueFormatter else { return [] }
+            let maxEntryCount = chart.data?.maxEntryCountSet?.entryCount ?? 0
+            return stride(from: 0, to: maxEntryCount, by: 1).map {
+                formatter.stringForValue(Double($0), axis: chart.xAxis)
+            }
+        }()
 
         // Make a tuple of (xLabels, value, originalIndex) then sort it
         // This is done, so that the labels are narrated in decreasing order of their corresponding value
         // Otherwise, there is no non-visual logic to the data presented
         let accessibilityEntryValues =  Array(0 ..< entryCount).map { (dataSet.entryForIndex($0)?.y ?? 0, $0) }
-        let accessibilityAxisLabelValueTuples = zip(accessibilityXLabels, accessibilityEntryValues).map { ($0, $1.0, $1.1) }.sorted { $0.1 > $1.1 }
+        let accessibilityAxisLabelValueTuples = zip(xLabels, accessibilityEntryValues).map { ($0, $1.0, $1.1) }.sorted { $0.1 > $1.1 }
         let accessibilityDataSetDescription: String = description + ". \(entryCount) \(prefix + (entryCount == 1 ? "" : "s")). "
         let accessibilityFrameWidth: CGFloat = 22.0 // To allow a tap target of 44x44
 
